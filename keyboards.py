b037ef8f1c46e813ebeab8e7175595726a7bd977
ilia <il.vsl0110@gmail.com>
2021-05-29 23:34:24 +0300

    add comments

--- keyboards.py
+++ keyboards.py
@@ -1,9 +1,14 @@
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
-
+'''
+класс клавиатур
+'''
 class Keyboard:
     @staticmethod
     def base_keyboard():
+        '''
+        клавиатура, появляющаяся в начале работы бота
+        '''
         keyboard = ReplyKeyboardMarkup()
         keyboard.add(KeyboardButton("Посмотреть корзину"))
         keyboard.add(KeyboardButton("Посмотреть меню"))
@@ -13,6 +18,9 @@ class Keyboard:
 
     @staticmethod
     def trash_keyboard():
+        '''
+        клавиатура, появляющаяся при переходе на просмотр корзины
+        '''
         keyboard = ReplyKeyboardMarkup()
         keyboard.add(KeyboardButton("Очистить корзину"))
         keyboard.add(KeyboardButton("Посмотреть стоимость"))
@@ -23,12 +31,18 @@ class Keyboard:
 
     @staticmethod
     def product_inline_keyboard(product_id:str, user_id:str):
+        '''
+        клавиатура появляющаяся при просмотре меню, нужна чтобы добавить текущий товар в корзину
+        '''
         keyboard = InlineKeyboardMarkup()
         keyboard.add(InlineKeyboardButton("Добавить в корзину", callback_data="add {} {}".format(product_id, user_id)))
         return keyboard
     
     @staticmethod
     def delete_inline_keyboard(product_id:str, user_id:str):
+        '''
+        клавиатура появляющаяся при просмотре корзины, нужна чтобы удалить текущий товар из корзины
+        '''
         keyboard = InlineKeyboardMarkup()
         keyboard.add(InlineKeyboardButton("Удалить из корзины", callback_data="delete {} {} ".format(product_id, user_id)))
         return keyboard
--- logic.py
+++ logic.py
@@ -3,10 +3,16 @@ from datetime import time
 
 
 def get_products():
+    '''
+    функция возвращающая все продукты
+    '''
     return Product.select()
 
 
 def get_trash_ids(user_id):
+    '''
+    функция возвращающая id продуктов, заказанных пользователем
+    '''
     trash = None,
     error = None
     try:
@@ -20,10 +26,16 @@ def get_trash_ids(user_id):
 
 
 def get_product(product_id):
+    '''
+    функция возвращающая продукт по его id
+    '''
     return Product.get(id=product_id)
 
 
 def remove_trash(user_id):
+    '''
+    функция очищающая корзину
+    '''
     message = "Удаление продукта прошло успешно"
     try:
         trash = History.get(name=user_id)
@@ -36,6 +48,9 @@ def remove_trash(user_id):
 
 
 def get_cost(user_id):
+    '''
+    функция, считающая стоимость корзины пользователя
+    '''
     try:
         trash = History.get(id=user_id)
         cost = 0
@@ -47,11 +62,17 @@ def get_cost(user_id):
 
 
 def get_list_of_orders():
+    '''
+    функция возвращает список заказов
+    '''
     orders = Order.select()
     return orders
 
 
 def create_order(user_id):
+    '''
+    функция создания заказа
+    '''
     try:
         trash = History.get(name=user_id)
         cost = 0
@@ -75,6 +96,9 @@ def create_order(user_id):
 
 
 def delete_from_trash(user_id, delete_product_id):
+    '''
+    функция удаления товара из корзины
+    '''
     trash = History.get(name=user_id)
 
     new_ids = []
@@ -90,6 +114,10 @@ def delete_from_trash(user_id, delete_product_id):
 
 
 def add_to_trash(user_id, add_product_id):
+    '''
+    функция добавления товара в корзину
+    '''
+
     trash = None
 
     try:
